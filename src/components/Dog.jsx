11a8f9419ba1702b3fc06541ad1322d05f511b02
Vaibhav Annaso Tembukade <vaibhavtembukadea09@gmail.com>
2026-03-07 19:12:02 +0530

    feat : normalMaps loaded on GLTF Dog 3D model

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import {Canvas, useThree} from '@react-three/fiber'
 import { DirectionalLight, Scene } from 'three'
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { OrbitControls, useGLTF, useTexture } from '@react-three/drei';
+import { color, If, normalMap } from 'three/tsl';
+import * as Three from 'three';
+
 
 function Dog() {
 
     const model = useGLTF("./models/dog.drc.glb")
     useThree(({camera, scene, gl})=>(
-        camera.position.z=0.7
+        camera.position.z=0.6
     ))
+
+    const textures=useTexture({
+        normalMap:"/dog_normals.jpg"
+    })
+
+    textures.normalMap.flipY=false
+
+    model.scene.traverse((child)=>{
+        if(child.name.includes("DOG")) {
+           child.material=new Three.MeshMatcapMaterial({
+            normalMap: textures.normalMap,
+            
+           })
+        }
+    })
   return (
         <>
-        <primitive object={model.scene} position={[0,-0.5,0]} rotation={[0,0, 0]}/>
+        <primitive object={model.scene} position={[0.25,-0.6,0]} rotation={[0,Math.PI/5, 0]}/>
         <directionalLight color={0xFFFFFF} intensity={10} position={[0,0,5]}/>
         <OrbitControls />
         </>
