e06ed57c7fd02f3699ae9bf38dc5eb937dd2d209
Vaibhav Annaso Tembukade <vaibhavtembukadea09@gmail.com>
2026-03-07 20:07:11 +0530

    fix: Colours

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Canvas, useThree} from '@react-three/fiber'
 import { DirectionalLight, Scene } from 'three'
-import { OrbitControls, useGLTF, useTexture } from '@react-three/drei';
+import { OrbitControls, useGLTF, useTexture, useAnimations } from '@react-three/drei';
 import { color, If, normalMap } from 'three/tsl';
 import * as Three from 'three';
 
@@ -10,26 +10,52 @@ function Dog() {
 
     const model = useGLTF("./models/dog.drc.glb")
     useThree(({camera, scene, gl})=>(
-        camera.position.z=0.6
+        camera.position.z=0.4,
+        gl.toneMapping = Three.ReinhardToneMapping,
+        gl.outputColorSpace=Three.SRGBColorSpace
     ))
 
-    const textures=useTexture({
-        normalMap:"/dog_normals.jpg"
-    })
+    const {actions} = useAnimations(model.animations,model.scene)
+    
+    useEffect(()=>{
+        actions["Take 001"].play()
+    },[actions])
 
-    textures.normalMap.flipY=false
+    /* const textures=useTexture({
+        normalMap:"/dog_normals.jpg",
+        sampleMapCap:"/matcap/mat-2.png"
+    },(texture)=>{
+        texture.flipY=false,
+        texture.colorSpace=Three.SRGBColorSpace
+    })
+     textures.flipY=false,
+    textures.colorSpace=Three.SRGBColorSpace    
+    */
+
+    const [
+        normalMap,
+        sampleMapCap
+    ]= (useTexture(["/dog_normals.jpg","/matcap/mat-2.png"])).map(
+        texture=>{
+        texture.flipY=false,
+        texture.colorSpace=Three.SRGBColorSpace
+        return texture
+    }
+    )
+
+    const dogMaterial=new Three.MeshMatcapMaterial({
+        normalMap: normalMap,
+        matcap: sampleMapCap
+    })
 
     model.scene.traverse((child)=>{
         if(child.name.includes("DOG")) {
-           child.material=new Three.MeshMatcapMaterial({
-            normalMap: textures.normalMap,
-            
-           })
+           child.material=dogMaterial
         }
     })
   return (
         <>
-        <primitive object={model.scene} position={[0.25,-0.6,0]} rotation={[0,Math.PI/5, 0]}/>
+        <primitive object={model.scene} position={[0.2,-0.6,0]} rotation={[0,Math.PI/5, 0]}/>
         <directionalLight color={0xFFFFFF} intensity={10} position={[0,0,5]}/>
         <OrbitControls />
         </>
